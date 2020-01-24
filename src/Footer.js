639228613dd64b9508fedb9e67694c34082bc694
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-24 09:58:47 -0700

    Add media queries

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-	return <footer></footer>;
+	return (
+		<footer>
+			<h5>Built by Edwin Del Bosque</h5>
+		</footer>
+	);
 };
 
 export default Footer;
