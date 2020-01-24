a70edd7232c11630e9584a806087813b84533288
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-24 11:03:41 -0700

    Add borders to structure design

--- src/Profile.js
+++ src/Profile.js
@@ -9,12 +9,13 @@ const Profile = ({ title, body, image, date }) => {
 				<h6>{date}</h6>
 				<h1>{title.toUpperCase()}</h1>
 				<img src={image} alt={title} />
-				<p>
-					<span>{body.slice(0, 1)}</span>
-					{body.slice(1)}
-				</p>
-				<p>{body}</p>
-				<p>{body}</p>
+				<main>
+					<p>
+						<span>{body.slice(0, 1)}</span>
+						{body.slice(1)}
+					</p>
+					<p>{body}</p>
+				</main>
 			</Fade>
 		</article>
 	);
