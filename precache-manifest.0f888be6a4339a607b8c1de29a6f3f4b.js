ad3865ec95c0e264c37cafd43897bc517f8d5063
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-24 10:02:20 -0700

    Updates

---
+++ precache-manifest.0f888be6a4339a607b8c1de29a6f3f4b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a30c3061fd730d5510dddf6905858ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "889cd24de3b0170301c4",
+    "url": "./static/css/main.75d3c5a0.chunk.css"
+  },
+  {
+    "revision": "bce0798deb7534965a52",
+    "url": "./static/js/2.f292428c.chunk.js"
+  },
+  {
+    "revision": "fc8bc21d8392cbf38ae828c75bee2e0a",
+    "url": "./static/js/2.f292428c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "889cd24de3b0170301c4",
+    "url": "./static/js/main.07c71069.chunk.js"
+  },
+  {
+    "revision": "91fc14e6815f774b1ade",
+    "url": "./static/js/runtime-main.ea9f8f44.js"
+  },
+  {
+    "revision": "a0a7da91ed74445035b95ebfa01c3234",
+    "url": "./static/media/1 - August 2016.a0a7da91.png"
+  },
+  {
+    "revision": "074820ea7442b82be8811bf126ce59cd",
+    "url": "./static/media/2 - December 2016.074820ea.jpg"
+  },
+  {
+    "revision": "aed5dcefe84622206aed9606b3be0980",
+    "url": "./static/media/3 - January 2017.aed5dcef.JPG"
+  },
+  {
+    "revision": "e69cac89851169d3584aa2db279a889a",
+    "url": "./static/media/4 - April 2017.e69cac89.png"
+  },
+  {
+    "revision": "f1311ffd2a53d4aca0e0bc84d469c11c",
+    "url": "./static/media/5 - August 2017.f1311ffd.jpg"
+  },
+  {
+    "revision": "fd84b10a3203806ef55e24785e63fbde",
+    "url": "./static/media/6 - February 2018.fd84b10a.jpg"
+  },
+  {
+    "revision": "d5a619928fd5d77affdc6c4ecaffb269",
+    "url": "./static/media/7 - August 2019.d5a61992.jpg"
+  }
+]);
