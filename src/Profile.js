8e94a2ce75479172dba9f006cac7d5c20cf785d0
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-24 11:31:22 -0700

    Polish paddings and margins

--- src/Profile.js
+++ src/Profile.js
@@ -6,15 +6,15 @@ const Profile = ({ title, body, image, date }) => {
 	return (
 		<article>
 			<Fade bottom spy={title} appear={true} distance='50px'>
-				<h6>{date}</h6>
+				<h6>{date.toUpperCase()}</h6>
 				<h1>{title.toUpperCase()}</h1>
-				<img src={image} alt={title} />
 				<main>
 					<p>
 						<span>{body.slice(0, 1)}</span>
 						{body.slice(1)}
 					</p>
 					<p>{body}</p>
+					<img src={image} alt={title} />
 				</main>
 			</Fade>
 		</article>
